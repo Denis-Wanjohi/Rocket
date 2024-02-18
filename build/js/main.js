cd4ed60c3c6f3c117f76a4d05d67fb330508b8f3
gightkingss <gightkingss@gmail.com>
2024-02-18 23:19:52 +0300

    rockets

---
+++ build/js/main.js
@@ -0,0 +1,18 @@
+const initApp = ()=>{
+    const hambugerBtn = document.getElementById('humbuger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const toggleMenu  =()=>{
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+        hambugerBtn.classList.toggle('toggle-btn')
+    }
+
+    hambugerBtn.addEventListener('click',toggleMenu)
+    mobileMenu.addEventListener('click',toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded',initApp)
+
+
+
